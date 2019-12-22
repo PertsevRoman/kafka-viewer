5bfee5b38a86b9ad8d79c0e25189a9935ad458be
Roman Pertsev <pertsev.roman@gmail.com>
2019-12-22 22:25:09 +0300

    change main class application

--- backend/src/main/java/com/ringcentral/kv/KafkaViewerApplication.java
+++ backend/src/main/java/com/ringcentral/kv/KafkaViewerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntelliDumpApplication {
+public class KafkaViewerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IntelliDumpApplication.class, args);
+        SpringApplication.run(KafkaViewerApplication.class, args);
     }
 
 }
