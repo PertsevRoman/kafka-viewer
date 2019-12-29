cea5020a4e2244c1d36d04f312df8f002afb9a8f
roman <pertsev.roman@gmail.com>
2019-12-29 18:47:06 +0300

    create kafka producer application

---
+++ backend/kafka-producer/src/main/java/com/kafka/viewer/KafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.viewer;
+
+/**
+ * Hello world!
+ *
+ */
+public class KafkaProducerApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
