3a2544e63657352ca3a967dbfa9edde3199cf183
Roman Pertsev <pertsev.roman@gmail.com>
2020-01-08 20:02:53 +0300

    add priveleged mode and start daemon

--- stack/dev/main.tf
+++ stack/dev/main.tf
@@ -100,6 +100,7 @@ resource "aws_codebuild_project" "code-build" {
     image                       = "aws/codebuild/standard:3.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
   }
 
   source {
